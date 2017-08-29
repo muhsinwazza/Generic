060175ba59672828d5d11c537f231563796dd1ab
Muhsin PK <muhsinwazza@gmail.com>
2017-08-30 03:18:59 +0530

    Login App see instruction
    From root folder do npm install (in cmd)
    then npm start -- to start app

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent { }
+
+//used local storaage ,we can stor4e and retrieve data with browser memory
